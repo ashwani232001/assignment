0a9f1e8b3f45e49b166baf7f04ab8d27c8fe7d09
ashwani232001 <122799822+ashwani232001@users.noreply.github.com>
2023-01-26 15:35:52 +0530

    Update Weekprogram.java

--- Weekprogram.java
+++ Weekprogram.java
@@ -1,34 +1,24 @@
 class Weekprogram {
 public static void main(String[]args){
- int n =5;
-
-
-        if (n==1){
+ int n = 5;
+ 
+        if (n == 1) {
             System.out.println("monday");
-        }
-        else if (n==2){
+        } else if (n == 2) {
             System.out.println("tuesday");
-        }
-        else if  (n==3){
+        } else if (n == 3) {
             System.out.println("wednesday");
-        }
-        else if (n==4){                                    
+        } else if (n == 4) {
             System.out.println("thursday");
-        }
-        else if (n==5) {
+        } else if (n == 5) {
             System.out.println("friday");
-        }
-        else if (n==6){
+        } else if (n == 6) {
             System.out.println("saturday");
-        }
-        else if (n==7)
+        } else if (n == 7) {
             System.out.println("sunday");
+        } else {
+            System.out.println("Enter the valid number");
         }
-         
-      
-    {
-        System.out.println("Enter the valid number");
-    }
-
+}
 }
 
